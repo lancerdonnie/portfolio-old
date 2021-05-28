b6d29a635d0550e9ac292f086e0b40953ac73682
lancerdonnie <jideadedejifirst@gmail.com>
2021-05-28 07:48:06 +0100

    changed title

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,15 +3,15 @@ import './Home.scss';
 import Arrow from './Arrow';
 const Home = ({ anim }) => {
   return (
-    <div className='home'>
-      <div className='homediv'>
+    <div className="home">
+      <div className="homediv">
         <span className={anim ? 'anim' : ''}>Hi</span>
         <br />
         <span className={anim ? 'anim' : ''}>I'm </span>
         <span className={anim ? 'anim' : ''}>adedeji babajide</span>
         <br />
         <span className={anim ? 'anim' : ''}>
-          a fullstack web developer in Lagos
+          a fullstack software engineer in Lagos
         </span>
         <br />
         <span className={anim ? 'anim' : ''}>
