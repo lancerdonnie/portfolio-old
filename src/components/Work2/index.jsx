2e3958d22afc06b4c5a98a080d336c4275a5623a
lancerdonnie <jideadedejifirst@gmail.com>
2021-06-05 11:08:41 +0100

    removed unecessary files

--- src/components/Work2/index.jsx
+++ src/components/Work2/index.jsx
@@ -1,22 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Work2.scss';
 
 const project = {
   name: 'Blanche Neige',
-  image:
+  images: [
     'https://res.cloudinary.com/dtyrfo0fx/image/upload/q_auto/v1581079144/portfolio/Blanche.png',
+    'https://res.cloudinary.com/dtyrfo0fx/image/upload/q_auto/v1581668520/portfolio/chat.png',
+    'https://res.cloudinary.com/dtyrfo0fx/image/upload/q_auto/v1581079141/portfolio/book.png',
+    'https://res.cloudinary.com/dtyrfo0fx/image/upload/q_auto/v1581079140/portfolio/events.png',
+  ],
   desc: 'An e-commerce website where you buy stuff and checkout',
   link: 'https://blancheneige.netlify.com/',
   git: 'https://github.com/lancerdonnie/blanche-neige',
+  tech: ['react', 'firebase'],
 };
 
-const index = () => {
+const Index = () => {
+  const [selected, setSelected] = useState(0);
   return (
     <div className="work2">
-      <div></div>
-      <div></div>
+      <div>Next</div>
+      <div>
+        <div className="work2__images">
+          {project.images.map((e, i) => (
+            <div
+              className={`${selected === i ? 'selected' : ''}`}
+              onClick={() => setSelected(i)}
+            >
+              <img src={e} alt={project.name + ' picture' + i} />
+            </div>
+          ))}
+        </div>
+        <div className="work2__body">
+          <div className="work2__left">
+            <img
+              src={project.images[selected]}
+              alt={project.name + ' picture'}
+            />
+          </div>
+          <div className="work2__right">
+            <div> {project.name}</div>
+            <div> {project.desc}</div>
+            <div className="tech">
+              {project.tech.map((e) => (
+                <span>{e}</span>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default index;
+export default Index;
